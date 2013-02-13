823e846b9b4e4388f10fda90e1cc2c959e05ad15
Jan Wikholm <jw@jw.fi>
2013-02-12 20:38:30 -0800

    more comments

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,23 @@
 
+int step = 20; // kokeile muuttaa tata.
+int framerate = 60; // taman asettaminen pienemmaksi saa taman nayttamaan lagaavalta. kokeile muuttaa esim 1, 10, 50, 100
+float gravity = 9.81f / framerate; /// fysiikkaa: painovoiman takia nopeus kasvaa 9.81 pikselia per sekunti joka sekunti
+                                   /// joten tarvitaan tietaa montako pikselia pitaa liikkua per frame
+
+//// alustetaan ohjelmaan oikea koko ja nopeus.
+void setup() {
+    size(800, 600);
+    frameRate(framerate);
+    stroke(0, 0, 0); // black
+    font = createFont("Courier New", 16);
+    textFont(font);
+}
+
+//// tata kutsutaan automaattisesti kokoajan.
+//// framerate kertoo kuinka monta kertaa sekunnissa
+//// elokuvat ovat yleensa 30 fps
+//// pelit toivottavasti yli 50 fps.
+//// tuossa ylla asetettu etta tama pyrkii olemaan 60 fps.
 
 void draw() {
     stroke(0, 0, 0); // 0 black
@@ -7,6 +26,8 @@ void draw() {
     drawGrid();
     drawCoordinates();
    
+    //// kaydaan jokainen ruudulla oleva pallo lapi ja katsotaan onko se oikeasti viela ruudulla
+    //// jos se on mennyt jo yli laidan niin voidaan poistaa se listalta, ettei turhaan vie muistia
     for (int i = balls.size()-1; i >= 0; i--) { 
 	    Ball ball = (Ball) balls.get(i);
 
@@ -20,10 +41,67 @@ void draw() {
 }
 
 
+//// koordinaatisto
+void drawGrid() {
+    drawMajorLines();
+    drawMinorLines();
+}
 
-int step = 20;
-int framerate = 60;
-float gravity = 9.81f / framerate; // 9.81 / s^2 ; framerate frames per second => increment by 1/framerate every frame;
+void drawMajorLines() {
+    strokeWeight(5f);
+    line(0, height / 2, width, height / 2); // X
+    line(width / 2, 0, width / 2, height); // Y
+}
+
+void drawMinorLines() {
+    strokeWeight(1f);
+    for (int x = step; x < width; x += step) {
+        line(x, 0, x, height);
+    }
+    for (int y = step; y < height; y += step) {
+        line(0, y, width, y);
+    }
+}
+
+void drawCoordinates() {
+    strokeWeight(1f);
+    // processing coordinates
+    fill(225, 225, 225); // grey
+    rect(0, 0, 80, 20);
+    fill(0, 0, 0); // black
+    text(mouseX + "," +mouseY, 5, 15);
+
+    // origo coordinates
+    fill(225, 225, 225); // grey
+    rect(width - 80, 0, 80, 20);
+    fill(0);
+    text(round((mouseX - (width / 2)) / step) + "," + round(((height / 2) - mouseY) / step), width - 75, 15);
+
+}
+
+void drawBallCount() {
+    stroke(255, 0, 0);
+    fill(225, 225, 225); // grey
+    rect(0, height - 20, 80, 20);
+    fill(0);
+    text(balls.size() + " balls", 5, height - 5);
+}
+
+
+void mouseClicked() {
+    balls.add(new Ball(mouseX, mouseY));
+}
+
+void mouseDragged() {
+    mouseClicked();
+}
+
+//////////////////////////////////////////////////////////////
+//////// 
+//////// Pallojen toiminnallisuus
+//////// sis. varityksen ja liikkeen (putoamisnopeus)
+////////
+////////////////////////////////////////////////////////
 
 ArrayList balls = new ArrayList();
 
@@ -73,65 +151,4 @@ class Ball {
     }
 }
 
-void setup() {
-    size(800, 600);
-    frameRate(framerate);
-    stroke(0, 0, 0); // black
-    font = createFont("Courier New", 16);
-    textFont(font);
-}
 
-
-void drawGrid() {
-    drawMajorLines();
-    drawMinorLines();
-}
-
-public void mouseClicked() {
-    balls.add(new Ball(mouseX, mouseY));
-}
-
-public void mouseDragged() {
-    mouseClicked();
-}
-
-
-void drawMajorLines() {
-    strokeWeight(5f);
-    line(0, height / 2, width, height / 2); // X
-    line(width / 2, 0, width / 2, height); // Y
-}
-
-void drawMinorLines() {
-    strokeWeight(1f);
-    for (int x = step; x < width; x += step) {
-        line(x, 0, x, height);
-    }
-    for (int y = step; y < height; y += step) {
-        line(0, y, width, y);
-    }
-}
-
-void drawCoordinates() {
-    strokeWeight(1f);
-    // processing coordinates
-    fill(225, 225, 225); // grey
-    rect(0, 0, 80, 20);
-    fill(0, 0, 0); // black
-    text(mouseX + "," +mouseY, 5, 15);
-
-    // origo coordinates
-    fill(225, 225, 225); // grey
-    rect(width - 80, 0, 80, 20);
-    fill(0);
-    text(round((mouseX - (width / 2)) / step) + "," + round(((height / 2) - mouseY) / step), width - 75, 15);
-
-}
-
-void drawBallCount() {
-    stroke(255, 0, 0);
-    fill(225, 225, 225); // grey
-    rect(0, height - 20, 80, 20);
-    fill(0);
-    text(balls.size() + " balls", 5, height - 5);
-}
